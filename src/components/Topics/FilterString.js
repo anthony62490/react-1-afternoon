9e2b42b0c3c9eb10f55be0f113d8299dbdc647cd
anthony62490 <anthonyjmagno@gmail.com>
2018-08-10 17:48:44 -0500

    Completed third puzzle and started on palindrome

--- src/components/Topics/FilterString.js
+++ src/components/Topics/FilterString.js
@@ -5,14 +5,40 @@ class FilterString extends Component
     constructor()
     {
         super();
-        this.state = {};
+        this.state = 
+        {
+            unfilteredArray: ['James', 'Jessica', 'Melody', 'Tyler', 'Blake', 'Jennifer', 'Mark', 'Maddy', 'Thaddeus'],
+            filteredArray: [],
+            userInput: ''
+        };
+    }
+
+    handleInput(str)
+    {
+        this.setState({userInput: str.target.value});
+    }
+
+    filterButtonEvent()
+    {
+        let strs = this.state.unfilteredArray;
+        let part = this.state.userInput;
+        let match = strs.filter(x => x.startsWith(part));
+        this.setState({filteredArray: match, userInput: ''});
     }
 
     render()
     {
         return (
-            <div className="puzzle">
-                <p>FilterString Component</p>
+            <div className="puzzleBox filterStringPB">
+                <h4>"Filter String"</h4>
+                <span className="puzzleText">
+                    {String(this.state.unfilteredArray)}
+                </span>
+                <input className="inputLine" onChange={(x)=> this.handleInput(x)} value={this.state.userInput} />
+                <button className="confirmationButton" onClick={()=> this.filterButtonEvent()}>Filter!</button>
+                <span className="resultsBox filterStringRB">
+                    {String(this.state.filteredArray)}
+                </span>
             </div>
         ); //return
     };
--- src/components/Topics/Palindrome.js
+++ src/components/Topics/Palindrome.js
@@ -5,14 +5,61 @@ class Palindrome extends Component
     constructor()
     {
         super();
-        this.state = {};
+        this.state = 
+        {
+            userInput: '',
+            palindrome: ''
+        };
     }
+
+    handleInput(input)
+    {
+        this.setState({userInput: input.target.value});
+    }
+
+    palindromeButtonEvent()
+    {
+        function isLetter(i) 
+        {
+            i.toLowerCase();
+            console.log("test ",i);
+            // return i.length !== 1 && i.match(/[A-Z|a-z]/i);
+            if (i.length !== 1)
+            {
+                console.log("check");
+                if(i.match(/[a-z|A-Z]/g)) 
+                    return true; 
+            }
+            return false;
+        }
+
+        // let check = this.state.userInput;
+        console.log(isLetter('a'));
+        console.log(isLetter('4'));
+        console.log(isLetter('z'));
+        console.log(isLetter('A'));
+        console.log(isLetter('Z'));
+        console.log(isLetter('/'));
+        console.log(isLetter('aa'));
+        //first, strip out everything that's not a letter and convert to lowercase
+        // for(let i=0; i<check.length; i++)
+        // {
+        //     if(check[i].charAt)
+        // }
+    }
+
+    
 
     render()
     {
         return (
-            <div className="puzzle">
-                <p>Palindrome Component</p>
+            <div className="puzzleBox filterStringPB">            
+                <h4>"Palindrome"</h4>
+                <input className="inputLine" onChange={(x)=> this.handleInput(x)} value={this.state.userInput}/>
+                <button className="confirmationButton" onClick={()=> this.palindromeButtonEvent()}>A nut for a jar of tuna.</button>
+                <span className="resultsBox">
+                    {this.state.palindrome}
+                </span>
             </div>
         ); //return
     };
