6d9b8fa84b8ec93ee7cb294f28fccce2e35765f4
anthony62490 <anthonyjmagno@gmail.com>
2018-08-12 00:43:14 -0500

    started on new puzzle

---
+++ src/components/Topics/TextBanner.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+
+class TextBanner extends Component()
+{
+    constructor()
+    {
+        super();
+        this.state = 
+        {
+            decoration = '#',
+            userInput = ''
+        };
+    }
+
+    render()
+    {
+        return(e); //return
+    }
+}
