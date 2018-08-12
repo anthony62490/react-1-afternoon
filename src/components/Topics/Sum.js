a3aa5839830f6d663e2a351c158ab428a31547b4
anthony62490 <anthonyjmagno@gmail.com>
2018-08-11 22:02:17 -0500

    Finished Sum puzzle

--- src/components/Topics/Sum.js
+++ src/components/Topics/Sum.js
@@ -17,7 +17,8 @@ class Sum extends Component
     {
         if(isOne)
             this.setState({num1: num.target.value});
-        this.setState({num2: num.target.value});
+        else
+            this.setState({num2: num.target.value});
 
         // console.log(`${this.state.num1} / ${this.state.num2}`);
     }
@@ -36,7 +37,7 @@ class Sum extends Component
     {
         let one = Number(this.state.num1);
         let two = Number(this.state.num2);
-        this.setState({sum: (one+two), num1: 0, num2: 0});
+        this.setState({sum: (one+two)/*, num1: '', num2: ''*/});
     }
 
     render()
@@ -44,8 +45,8 @@ class Sum extends Component
         return (
             <div className="puzzleBox sumPB">
                 <h4>"Sum"</h4>
-                <input className="inputLine" onChange={(x)=> this.handleInput(x, true)} value={this.handleEmptyInput(1)} placeholder={`1st number`}/>
-                <input className="inputLine" onChange={(x)=> this.handleInput(x, false)} value={this.handleEmptyInput(2)} placeholder={`2nd number`}/>
+                <input className="inputLine" onChange={(x)=> this.handleInput(x, true)} value={this.state.value} placeholder={`1st number`}/>
+                <input className="inputLine" onChange={(x)=> this.handleInput(x, false)} value={this.state.value} placeholder={`2nd number`}/>
                 <button className="confirmationButton" onClick={()=> this.buttonSumEvent()}>Fusion... HA!</button>
                 <span className="resultsBox" placeholder={`Sum`}>{this.state.sum}</span>
             </div>
