5cac954bb5de2d83adf91ede09c8ba6fc3770c1e
anthony62490 <anthonyjmagno@gmail.com>
2018-08-12 11:55:53 -0500

    BROKEN Working on TextBanner module

--- src/components/Topics/TextBanner.js
+++ src/components/Topics/TextBanner.js
@@ -8,12 +8,38 @@ class TextBanner extends Component()
         this.state = 
         {
             decoration = '#',
-            userInput = ''
+            userInput = '',
+            banner = ''
         };
+    }
+
+    makeBanner()
+    {
+        if(this.state.decoration != '' && this.state.userInput != '') //banner will only show if there is decoration
+        {
+            let str = String(this.state.userInput); //prevent strange things when the user inputs numbers
+            let x = String(this.state.decoration);
+            let topBot = Array(str.length+2).join(x);
+            let mid = x+str+x;
+
+        }
+        else //if either piece is missing, display first column of decoration, even if it == ''
+        {
+            this.setState( {banner: `${this.state.decoration}
+            ${this.state.decoration}
+            ${this.state.decoration}`} );
+        }
     }
 
     render()
     {
-        return(e); //return
+        return(
+            <div className="puzzleBox filterObjectPB">
+                <h4>"Text Banner"</h4>
+                <span className="puzzleText"> {this.makeBanner()} </span>
+                <input className="inputLine" />
+                <input className="inputLine"/>
+            </div>
+        ); //return
     }
 }
